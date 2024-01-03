a95b2fb3762bae44b0d392622c090e84130d14b5
CalvinKarume <johncalvinkarume@gmail.com>
2024-01-03 10:32:47 -0800

    BUG FIXING

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,2 @@
-#!/usr/bin/python3
-""" BasicCache module """
-from base_caching import BaseCaching
-
-class BasicCache(BaseCaching):
-    """ BasicCache class inherits from BaseCaching.
-    Implements a simple caching system without size limits.
-    """
-
-    def put(self, key, item):
-        """ Add item to cache.
-        If key or item is None, no action is taken.
-        """
-        if key and item:
-            self.cache_data[key] = item
-
-    def get(self, key):
-        """ Retrieve item from cache.
-        Returns value for key or None if key is None or not found.
-        """
-        if key and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+[master 9340de9] SMALL BUG
+ 1 file changed, 1 deletion(-)
